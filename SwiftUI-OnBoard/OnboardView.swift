a560b15f433e14db9c198b646345f8fe6d0d6101
Bekzhan Talgat <bekzhantalgat01@gmail.com>
2023-08-12 17:18:28 +0300

    Onboard card view outer block

--- SwiftUI-OnBoard/OnboardView.swift
+++ SwiftUI-OnBoard/OnboardView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftUIX
 
 struct OnboardView: View {
     var body: some View {
@@ -51,38 +52,107 @@ struct ContentView_Previews: PreviewProvider {
 
 struct OnboardingCardView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            Text("120 hours of videos")
-                .font(.footnote)
-                .fontWeight(.semibold)
-                .foregroundColor(.white.opacity(0.7))
-            
-            LinearGradient(
-                gradient: .init(
-                    stops: [
-                        .init(color: Color(hex: "FF8989"), location: 0),
-                        .init(color: Color(hex: "C8DEFF"), location: 0.5),
-                        .init(color: Color(hex: "369DFC"), location: 1),
-                    ]
-                ),
-                startPoint: .init(x: 0, y: 0),
-                endPoint: .init(x: 1, y: 1)
+        VStack {
+            VStack(alignment: .leading, spacing: 16) {
+                Text("120 hours of videos")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white.opacity(0.7))
+                
+                LinearGradient(
+                    gradient: .init(
+                        stops: [
+                            .init(color: Color(hex: "FF8989"), location: 0),
+                            .init(color: Color(hex: "C8DEFF"), location: 0.5),
+                            .init(color: Color(hex: "369DFC"), location: 1),
+                        ]
+                    ),
+                    startPoint: .init(x: 0, y: 0),
+                    endPoint: .init(x: 1, y: 1)
+                )
+                .frame(height: 140)
+                .mask(
+                    Text("Learn \ndesign \nand code")
+                        .font(.largeTitle)
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                )
+                
+                Text("Learn by building real apps using Swift, React, Flutter and more. Design files and source codes are included for each course")
+                    .font(.subheadline)
+                    .foregroundColor(.white.opacity(0.7))
+            }
+            .padding(30)
+            .background(
+                LinearGradient(
+                    gradient: Gradient(
+                        stops: [
+                            .init(color: Color(red: 0.1, green: 0.1, blue: 0.3, opacity: 1), location: 0),
+                            .init(color: Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0), location: 1),
+                        ]
+                    ),
+                    startPoint: .init(x: 0, y: 0.0),
+                    endPoint: .init(x: 0, y: 0.95)
+                )
             )
-            .frame(height: 140)
             .mask(
-                Text("Learn \ndesign \nand code")
-                    .font(.largeTitle)
-                    .bold()
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                RoundedRectangle(cornerRadius: 30, style: .continuous)
             )
-            
-            
-            
-            Text("Learn by building real apps using Swift, React, Flutter and more. Design files and source codes are included for each course")
-                .font(.subheadline)
-                .foregroundColor(.white.opacity(0.7))
+            .overlay(
+                RoundedRectangle(cornerRadius: 30, style: .continuous)
+                    .stroke(
+                        LinearGradient(
+                            gradient: .init(
+                                stops: [
+                                    .init(color: .init(hex: "F0FFFFFF"), location: 0),
+                                    .init(color: .init(hex: "00FFFFFF"), location: 1),
+                                ]
+                            ),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 1
+                    )
+                    .blendMode(.overlay)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .stroke(
+                                LinearGradient(
+                                    gradient: .init(
+                                        stops: [
+                                            .init(color: .init(hex: "F0FFFFFF"), location: 0),
+                                            .init(color: .init(hex: "00FFFFFF"), location: 1),
+                                        ]
+                                    ),
+                                    startPoint: .top,
+                                    endPoint: .bottom
+                                ),
+                                lineWidth: 3
+                            )
+                            .blur(radius: 10)
+                    )
+            )
+            .background(
+                VisualEffectBlurView(blurStyle: .systemUltraThinMaterialDark)
+                    .mask(
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .fill(
+                                LinearGradient(
+                                    gradient: .init(
+                                        colors: [
+                                            .white,
+                                            .white,
+                                            .clear
+                                        ]),
+                                    startPoint: .top,
+                                    endPoint: .bottom
+                                )
+                            )
+                    )
+            )
+            .padding(20)
         }
-        .padding(30)
+        .padding(.bottom, 60)
         .background(
             LinearGradient(
                 gradient: Gradient(
@@ -91,12 +161,60 @@ struct OnboardingCardView: View {
                         .init(color: Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0), location: 1),
                     ]
                 ),
-                startPoint: .init(x: 0, y: 0.0),
-                endPoint: .init(x: 0, y: 0.95)
+                startPoint: .bottom,
+                endPoint: .init(x: 0.5, y: 0.05)
             )
         )
         .mask(
             RoundedRectangle(cornerRadius: 30, style: .continuous)
+        )
+        .overlay(
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .stroke(
+                    LinearGradient(
+                        gradient: .init(
+                            stops: [
+                                .init(color: .init(hex: "F0FFFFFF"), location: 0),
+                                .init(color: .init(hex: "00FFFFFF"), location: 1),
+                            ]
+                        ),
+                        startPoint: .bottom,
+                        endPoint: .top
+                    ),
+                    lineWidth: 1
+                )
+                .blendMode(.overlay)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        .stroke(
+                            LinearGradient(
+                                gradient: .init(
+                                    stops: [
+                                        .init(color: .init(hex: "F0FFFFFF"), location: 0),
+                                        .init(color: .init(hex: "00FFFFFF"), location: 1),
+                                    ]
+                                ),
+                                startPoint: .bottom,
+                                endPoint: .top
+                            ),
+                            lineWidth: 3
+                        )
+                        .blur(radius: 10)
+                )
+        )
+        .background(
+            VisualEffectBlurView(blurStyle: .systemUltraThinMaterialDark)
+                .mask(
+                    RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        .fill(
+                            LinearGradient(
+                                gradient: .init(colors: [.white, .white, .clear]),
+                                startPoint: .bottom,
+                                endPoint: .top
+                            )
+                        )
+                )
+//                .blur(radius: 30)
         )
         .padding(20)
     }
